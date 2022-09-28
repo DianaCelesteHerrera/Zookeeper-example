b160821c62c3938dd1a939d9a721b2482ea5f685
DianaCelesteHerrera <dayanceles.com>
2022-09-27 21:14:39 -0500

    Teste/Tarea

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -13,6 +13,38 @@ class TestZookeeper(unittest.TestCase):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
 
+    def test_versiones(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.delete('/node1', 1)
+    
+    def test_exists(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.exist('/node1')
+
+    def test_show_nodes(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.create('/node2', 'algo', True, True, 10, '/node1')
+        tree.showTree()
+    
+    def test_crear_multinodes(self):
+        tree = Ztree()
+        tree.create('/node1', 'algo', True, True, 10, '/')
+        tree.create('/node2', 'algo', True, True, 10, '/')
+        tree.create('/node3', 'algo', True, True, 10, '/')
+        tree.create('/node4', 'algo', True, True, 10, '/')
+        tree.create('/node5', 'algo', True, True, 10, '/')
+        tree.create('/node6', 'algo', True, True, 10, '/')
+        tree.create('/node7', 'algo', True, True, 10, '/')
+        self.assertEqual(tree.getData('/node2'), 'algo')
+
+    def test_crear_znode_error(self):
+        tree = Ztree()
+        tree.create('/node1/node2/node3', 'algo', True, True, 10, '/')
+        self.assertEqual(tree.getData('/node1'), 'algo')
+
 
 if __name__ == '__main__':
     unittest.main()
